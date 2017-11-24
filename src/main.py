78bdf59b4be5b7f56bc9e757ad14289ba3269cf9
= <nan.wang@htouhui.com>
2017-11-24 23:12:32 +0800

    多线程执行

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ import time
 import oss2
 import os
 import logging
+import threading
 
 #########################################################################################################################################
 """
@@ -152,11 +153,11 @@ def down_to_local(file_path, local_file_path):
 """
 
 
-def execute():
-    logger.info('raise money start ...')
+def raise_200():
+    logger.info('raise money 200 start ...')
     total_num, success_num, failure_num = 0, 0, 0
     error_record = pd.DataFrame(columns=[u'手机号', u'额度'])
-    file_list = ['../resource/100.xls', '../resource/200.xls', '../resource/300.xls', '../resource/400.xls', '../resource/500.xls', ]
+    file_list = ['../resource/200.xls', ]
     for f in file_list:
         logger.info(f)
         quota = int(f[-7:-4])
@@ -182,7 +183,7 @@ def execute():
 
                 if raise_result is None:
                     raise_sql = 'insert into `pdl_user_raise_amount` (user_id, mobile, amount, period ) values (%s, %s, %s, %s)'
-                    param = (user['id'][0], mobile, 1000 + quota, '14, 21')
+                    param = (user['id'][0], mobile, 1000 + quota, '14,21')
                     insert(raise_sql, param)
                 else:
                     raise_sql = 'UPDATE `pdl_user_raise_amount` SET `amount` = amount + %s WHERE `user_id` = %s'
@@ -196,12 +197,170 @@ def execute():
                 error_record.loc[error_record.shape[0] + 1] = {u'手机号': mobile, u'额度': quota}
                 logger.error('%s raise amount failure, amount : %s, mobile : %s, error : %s', *(user['id'][0], quota, mobile, e))
 
-    logger.info('raise money end, total count : %s, success count : %s, failure count : %s', *(total_num, success_num, failure_num))
-    error_record.to_csv('../raise_amount_error.csv', mode='a+', encoding='utf-8', header=True, index=False, index_label=None)
+    logger.info('raise money 200 end, total count : %s, success count : %s, failure count : %s', *(total_num, success_num, failure_num))
+    error_record.to_csv('../raise_amount_200_error.csv', mode='a+', encoding='utf-8', header=True, index=False, index_label=None)
+
+
+def raise_300():
+    logger.info('raise money 300 start ...')
+    total_num, success_num, failure_num = 0, 0, 0
+    error_record = pd.DataFrame(columns=[u'手机号', u'额度'])
+    file_list = ['../resource/300.xls', ]
+    for f in file_list:
+        logger.info(f)
+        quota = int(f[-7:-4])
+        logger.info('start raise 300 money %s ...', quota)
+
+        data = pd.read_excel(f, sheetname='Sheet1', skiprows=0)
+        for i in range(len(data)):
+            mobile = data[u'手机号'][i]
+            total_num += 1
+
+            user_sql = 'SELECT `id` FROM `pdl_user_basic` WHERE `mobile` = %s' % mobile
+            user = select(user_sql)
+
+            try:
+                if user is None:
+                    failure_num += 1
+                    error_record.loc[error_record.shape[0] + 1] = {u'手机号': mobile, u'额度': quota}
+                    logger.error("could't find user with the phone number is %s", mobile)
+                    continue
+
+                raised_sql = 'SELECT * FROM `pdl_user_raise_amount` WHERE `mobile` = %s' % mobile
+                raise_result = select(raised_sql)
+
+                if raise_result is None:
+                    raise_sql = 'insert into `pdl_user_raise_amount` (user_id, mobile, amount, period ) values (%s, %s, %s, %s)'
+                    param = (user['id'][0], mobile, 1000 + quota, '14,21')
+                    insert(raise_sql, param)
+                else:
+                    raise_sql = 'UPDATE `pdl_user_raise_amount` SET `amount` = amount + %s WHERE `user_id` = %s'
+                    param = (quota, user['id'][0])
+                    insert(raise_sql, param)
+
+                success_num += 1
+                logger.info('%s raise amount success, amount : %s, mobile : %s' % (user['id'][0], quota, mobile))
+            except Exception, e:
+                failure_num += 1
+                error_record.loc[error_record.shape[0] + 1] = {u'手机号': mobile, u'额度': quota}
+                logger.error('%s raise amount failure, amount : %s, mobile : %s, error : %s', *(user['id'][0], quota, mobile, e))
+
+    logger.info('raise money 300 end, total count : %s, success count : %s, failure count : %s', *(total_num, success_num, failure_num))
+    error_record.to_csv('../raise_amount_300_error.csv', mode='a+', encoding='utf-8', header=True, index=False, index_label=None)
+
+
+def raise_400():
+    logger.info('raise money 400 start ...')
+    total_num, success_num, failure_num = 0, 0, 0
+    error_record = pd.DataFrame(columns=[u'手机号', u'额度'])
+    file_list = ['../resource/400.xls', ]
+    for f in file_list:
+        logger.info(f)
+        quota = int(f[-7:-4])
+        logger.info('start raise 400 money %s ...', quota)
+
+        data = pd.read_excel(f, sheetname='Sheet1', skiprows=0)
+        for i in range(len(data)):
+            mobile = data[u'手机号'][i]
+            total_num += 1
+
+            user_sql = 'SELECT `id` FROM `pdl_user_basic` WHERE `mobile` = %s' % mobile
+            user = select(user_sql)
+
+            try:
+                if user is None:
+                    failure_num += 1
+                    error_record.loc[error_record.shape[0] + 1] = {u'手机号': mobile, u'额度': quota}
+                    logger.error("could't find user with the phone number is %s", mobile)
+                    continue
+
+                raised_sql = 'SELECT * FROM `pdl_user_raise_amount` WHERE `mobile` = %s' % mobile
+                raise_result = select(raised_sql)
+
+                if raise_result is None:
+                    raise_sql = 'insert into `pdl_user_raise_amount` (user_id, mobile, amount, period ) values (%s, %s, %s, %s)'
+                    param = (user['id'][0], mobile, 1000 + quota, '14,21')
+                    insert(raise_sql, param)
+                else:
+                    raise_sql = 'UPDATE `pdl_user_raise_amount` SET `amount` = amount + %s WHERE `user_id` = %s'
+                    param = (quota, user['id'][0])
+                    insert(raise_sql, param)
+
+                success_num += 1
+                logger.info('%s raise amount success, amount : %s, mobile : %s' % (user['id'][0], quota, mobile))
+            except Exception, e:
+                failure_num += 1
+                error_record.loc[error_record.shape[0] + 1] = {u'手机号': mobile, u'额度': quota}
+                logger.error('%s raise amount failure, amount : %s, mobile : %s, error : %s', *(user['id'][0], quota, mobile, e))
+
+    logger.info('raise money 400 end, total count : %s, success count : %s, failure count : %s', *(total_num, success_num, failure_num))
+    error_record.to_csv('../raise_amount_400_error.csv', mode='a+', encoding='utf-8', header=True, index=False, index_label=None)
+
+
+def raise_500():
+    logger.info('raise money 500 start ...')
+    total_num, success_num, failure_num = 0, 0, 0
+    error_record = pd.DataFrame(columns=[u'手机号', u'额度'])
+    file_list = ['../resource/500.xls', ]
+    for f in file_list:
+        logger.info(f)
+        quota = int(f[-7:-4])
+        logger.info('start raise 500 money %s ...', quota)
+
+        data = pd.read_excel(f, sheetname='Sheet1', skiprows=0)
+        for i in range(len(data)):
+            mobile = data[u'手机号'][i]
+            total_num += 1
+
+            user_sql = 'SELECT `id` FROM `pdl_user_basic` WHERE `mobile` = %s' % mobile
+            user = select(user_sql)
+
+            try:
+                if user is None:
+                    failure_num += 1
+                    error_record.loc[error_record.shape[0] + 1] = {u'手机号': mobile, u'额度': quota}
+                    logger.error("could't find user with the phone number is %s", mobile)
+                    continue
+
+                raised_sql = 'SELECT * FROM `pdl_user_raise_amount` WHERE `mobile` = %s' % mobile
+                raise_result = select(raised_sql)
+
+                if raise_result is None:
+                    raise_sql = 'insert into `pdl_user_raise_amount` (user_id, mobile, amount, period ) values (%s, %s, %s, %s)'
+                    param = (user['id'][0], mobile, 1000 + quota, '14,21')
+                    insert(raise_sql, param)
+                else:
+                    raise_sql = 'UPDATE `pdl_user_raise_amount` SET `amount` = amount + %s WHERE `user_id` = %s'
+                    param = (quota, user['id'][0])
+                    insert(raise_sql, param)
+
+                success_num += 1
+                logger.info('%s raise amount success, amount : %s, mobile : %s' % (user['id'][0], quota, mobile))
+            except Exception, e:
+                failure_num += 1
+                error_record.loc[error_record.shape[0] + 1] = {u'手机号': mobile, u'额度': quota}
+                logger.error('%s raise amount failure, amount : %s, mobile : %s, error : %s', *(user['id'][0], quota, mobile, e))
+
+    logger.info('raise money 500 end, total count : %s, success count : %s, failure count : %s', *(total_num, success_num, failure_num))
+    error_record.to_csv('../raise_amount_500_error.csv', mode='a+', encoding='utf-8', header=True, index=False, index_label=None)
+
+
+threads = []
+t1 = threading.Thread(target=raise_200)
+threads.append(t1)
+t2 = threading.Thread(target=raise_300)
+threads.append(t2)
+t3 = threading.Thread(target=raise_400)
+threads.append(t3)
+t4 = threading.Thread(target=raise_500)
+threads.append(t4)
+
 
 #########################################################################################################################################
 """
     执行入口
 """
 if __name__ == '__main__':
-    execute()
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
