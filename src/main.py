1a7e5124707637ac0cea0ae069ba0b3a8e23b4b8
= <nan.wang@htouhui.com>
2017-11-27 14:42:34 +0800

    测试多进程

--- src/main.py
+++ src/main.py
@@ -150,60 +150,39 @@ def down_to_local(file_path, local_file_path):
 #########################################################################################################################################
 """
     逻辑代码
+    测试多线程
 """
 
+import time
+import threading
+from multiprocessing import Pool
+import os, time, random
 
-def raise_amount():
-    logger.info('raise money start ...')
-    total_num, success_num, failure_num = 0, 0, 0
-    error_record = pd.DataFrame(columns=[u'手机号', u'额度'])
-    file_list = ['../resource/300.xls', '../resource/200.xls', ]
-    for f in file_list:
-        logger.info(f)
-        quota = int(f[-7:-4])
-        logger.info('start raise money %s ...', quota)
 
-        data = pd.read_excel(f, sheetname='Sheet1', skiprows=0)
-        for i in range(len(data)):
-            mobile = data[u'手机号'][i]
-            total_num += 1
+def thread_1():
+    for i in range(0, 100):
+        logger.info('thread - 1 : %s ', i)
+        time.sleep(2)
 
-            user_sql = 'SELECT `id` FROM `pdl_user_basic` WHERE `mobile` = %s' % mobile
-            user = select(user_sql)
 
-            try:
-                if user is None:
-                    failure_num += 1
-                    error_record.loc[error_record.shape[0] + 1] = {u'手机号': mobile, u'额度': quota}
-                    logger.error("could't find user with the phone number is %s", mobile)
-                    continue
+def thread_2():
+    for i in range(0, 100):
+        logger.info('thread - 2 : %s ', i)
+        time.sleep(4)
 
-                raised_sql = 'SELECT * FROM `pdl_user_raise_amount` WHERE `mobile` = %s' % mobile
-                raise_result = select(raised_sql)
-
-                if raise_result is None:
-                    raise_sql = 'insert into `pdl_user_raise_amount` (user_id, mobile, amount, period ) values (%s, %s, %s, %s)'
-                    param = (user['id'][0], mobile, 1000 + quota, '14,21')
-                    insert(raise_sql, param)
-                else:
-                    raise_sql = 'UPDATE `pdl_user_raise_amount` SET `amount` = amount + %s WHERE `user_id` = %s'
-                    param = (quota, user['id'][0])
-                    insert(raise_sql, param)
-
-                success_num += 1
-                logger.info('%s raise amount success, amount : %s, mobile : %s' % (user['id'][0], quota, mobile))
-            except Exception, e:
-                failure_num += 1
-                error_record.loc[error_record.shape[0] + 1] = {u'手机号': mobile, u'额度': quota}
-                logger.error('%s raise amount failure, amount : %s, mobile : %s, error : %s', *(user['id'][0], quota, mobile, e))
-
-    logger.info('raise money end, total count : %s, success count : %s, failure count : %s', *(total_num, success_num, failure_num))
-    error_record.to_csv('../raise_amount_error.csv', mode='a+', encoding='utf-8', header=True, index=False, index_label=None)
-
+threads = []
+t1 = threading.Thread(target=thread_1)
+threads.append(t1)
+t2 = threading.Thread(target=thread_2)
+threads.append(t2)
 
 #########################################################################################################################################
 """
     执行入口
 """
 if __name__ == '__main__':
-    raise_amount()
+    p = Pool()
+    p.apply_async(thread_1)
+    p.apply_async(thread_2)
+    p.close()
+    p.join()
